0dddd67e7d02ade90a397b8ac87073aa77e13d10
Arvikdb17 <128912243+Arvikdb17@users.noreply.github.com>
2024-07-24 16:12:22 +0530

    Update IfStatement.java

--- IfStatement.java
+++ IfStatement.java
@@ -2,6 +2,7 @@ package ScopesOperators;
 
 public class IfStatement {
     public  static void main(String[] args){
+      //  Write a program with an if statement. Declare one variable inside the if block and another one outside. Print both variables inside and outside the if block to understand variable scope. (Comment the compiler error in your program while submitting)
         int a=10;
         if(true){
             int b=9;
